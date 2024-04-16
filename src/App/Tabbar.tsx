0890175419d57543e4c83837cf8a9ae749705af9
taDaota <143489790+taDaota@users.noreply.github.com>
2024-04-16 23:21:41 +0900

    Update Tabbar.tsx
    「マップについて」をコメントアウト

--- src/App/Tabbar.tsx
+++ src/App/Tabbar.tsx
@@ -6,17 +6,17 @@ import { FaList, FaHome, FaSearch, FaCamera } from "react-icons/fa"
 import { AiOutlineAppstore } from "react-icons/ai"
 
 const Content = () => {
-  return (
-    <div className="tabbar">
-      <ul>
-        <li><Link to="/"><div className="icon"><FaHome /></div><div className="text">ホーム</div></Link></li>
-        <li><Link to="/list"><div className="icon"><FaList /></div><div className="text">一覧</div></Link></li>
-        <li><Link to="/category"><div className="icon"><FaSearch /></div><div className="text">カテゴリ</div></Link></li>
-        <li><Link to="/images"><div className="icon"><FaCamera /></div><div className="text">写真から探す</div></Link></li>
-        <li><Link to="/about"><div className="icon"><AiOutlineAppstore /></div><div className="text">マップについて</div></Link></li>
-      </ul>
-    </div>
-  );
+  return (
+    <div className="tabbar">
+      <ul>
+        <li><Link to="/"><div className="icon"><FaHome /></div><div className="text">ホーム</div></Link></li>
+        <li><Link to="/list"><div className="icon"><FaList /></div><div className="text">一覧</div></Link></li>
+        <li><Link to="/category"><div className="icon"><FaSearch /></div><div className="text">カテゴリ</div></Link></li>
+        <li><Link to="/images"><div className="icon"><FaCamera /></div><div className="text">写真から探す</div></Link></li>
+        // 「マップについて」のリンクを削除しました
+      </ul>
+    </div>
+  );
 };
 
 export default Content;
