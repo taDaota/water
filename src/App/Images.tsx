b809aaa7e28def2e0fa751b6136254779d4e495a
taDaota <143489790+taDaota@users.noreply.github.com>
2024-04-18 15:46:37 +0900

    Update Images.tsx

--- src/App/Images.tsx
+++ src/App/Images.tsx
@@ -1,74 +1,66 @@
-import { useEffect, useState } from 'react'
-import { ImageList, ImageListItem } from '@material-ui/core'
-import Shop from './Shop'
-import './Images.scss'
+import React, { useEffect, useState } from 'react';
+import { ImageList, ImageListItem } from '@material-ui/core';
+import Shop from './Shop';
+import './Images.scss';
 
 type Props = {
   data: Pwamap.ShopData[];
 }
 
 const Content = (props: Props) => {
-
   const { data } = props;
-  const [validImageList, setValidImageList ] = useState<JSX.Element[]>([]);
-  const [shop, setShop] = useState<Pwamap.ShopData | undefined>()
+  const [validImageList, setValidImageList] = useState<JSX.Element[]>([]);
+  const [shop, setShop] = useState<Pwamap.ShopData | undefined>();
 
   const popupHandler = (shop: Pwamap.ShopData) => {
     if (shop) {
-      setShop(shop)
+      setShop(shop);
     }
-  }
+  };
 
   const closeHandler = () => {
-    setShop(undefined)
-  }
+    setShop(undefined);
+  };
 
   useEffect(() => {
-
-    let imageListItems = []
+    let imageListItems = [];
 
     for (let i = 0; i < data.length; i++) {
       const item = data[i];
-
       if (item['画像']) {
-
         imageListItems.push(
-          <ImageListItem
-            key={i}
-            className="mui-image-list-item"
-          >
+          <ImageListItem key={i} className="mui-image-list-item">
             <img
               src={item['画像']}
               alt={item['スポット名']}
               loading="lazy"
               onClick={() => popupHandler(item)}
-              onError={e => {
+              onError={(e) => {
                 //@ts-ignore
-                e.target.parentNode.remove()
+                e.target.parentNode.remove();
               }}
             />
           </ImageListItem>
-        )
+        );
       }
     }
 
-    setValidImageList(imageListItems)
-
-  }, [data])
+    setValidImageList(imageListItems);
+  }, [data]);
 
   return (
     <>
       <div className="head"></div>
       <div className="images">
         <div className="container">
-          <ImageList id="mui-image-list" sx={{ width: "100%", height: "100%" }} cols={2} rowHeight={164}>
+          <ImageList id="mui-image-list" sx={{ width: "100%", height: "auto" }} cols={2} rowHeight={164}>
             {validImageList}
           </ImageList>
-          {shop ?
+          {shop ? (
             <Shop shop={shop} close={closeHandler} />
-            :
+          ) : (
             <></>
-          }
+          )}
         </div>
       </div>
     </>
