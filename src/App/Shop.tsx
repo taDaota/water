6a6fba993f6d0b39ea9a6a33d0873f282dc77f46
taDaota <143489790+taDaota@users.noreply.github.com>
2024-04-18 16:27:14 +0900

    Update Shop.tsx

--- src/App/Shop.tsx
+++ src/App/Shop.tsx
@@ -16,17 +16,15 @@ const Content = (props: Props) => {
   const { shop } = props
 
   const clickHandler = () => {
-    props.close()
+    props.close();
     if(mapNode.current) {
-      mapNode.current.remove()
-      map.remove()
+      mapNode.current.remove();
+      if (map) map.remove();
     }
   }
 
   React.useEffect(() => {
-    if (!mapNode.current) {
-      return
-    }
+    if (!mapNode.current) return;
 
     // @ts-ignore
     const nextMap = new window.geolonia.Map({
@@ -35,27 +33,23 @@ const Content = (props: Props) => {
       zoom: 14,
       style: `geolonia/gsi`,
     });
-    setMap(nextMap)
+    setMap(nextMap);
   }, [shop, mapNode])
 
-  const distanceTipText = makeDistanceLabelText(shop.distance)
-  const category = shop['カテゴリ']
-  const content = shop['紹介文']
+  const distanceTipText = makeDistanceLabelText(shop.distance);
+  const category = shop['カテゴリ'];
+  const content = shop['紹介文'];
 
   const toBreakLine = (text: string) => {
-
     return text.split(/(\r\n)|(\n)|(\r)/g).map((line, i) => {
-
       let result: any = '';
-
       if (line === '\r\n' || line === '\n' || line === '\r') {
-        result = <br key={i} />
+        result = <br key={i} />;
       } else if (line !== undefined) {
-        result = line
+        result = line;
       }
-
-      return result
-    })
+      return result;
+    });
   }
 
   return (
@@ -63,8 +57,8 @@ const Content = (props: Props) => {
       <div className="head">
         <button onClick={clickHandler}><AiOutlineClose size="16px" color="#FFFFFF" /> 閉じる</button>
       </div>
-      <div className="container">
-        {shop?
+      <div className="container" style={{ minHeight: '200px', overflow: 'auto' }}>
+        {shop ?
           <>
             <h2>{shop['スポット名']}</h2>
             <div>
@@ -73,25 +67,24 @@ const Content = (props: Props) => {
                   <span onClick={clickHandler} className="category">{category}</span>
                 </Link>
               </span>
-              <span className="nowrap">{distanceTipText && <span className="distance">現在位置から {distanceTipText}</span> }</span>
+              <span className="nowrap">{distanceTipText && <span className="distance">現在位置から {distanceTipText}</span>}</span>
             </div>
 
-            <div style={{margin: "24px 0"}}><Links data={shop} /></div>
+            <div style={{ margin: "24px 0" }}><Links data={shop} /></div>
 
-            { shop['画像'] && <img src={shop['画像']} alt={shop['スポット名']} style={{width: "100%"}} />}
+            { shop['画像'] && <img src={shop['画像']} alt={shop['スポット名']} style={{ width: "100%" }} />}
 
-            <p style={{margin: "24px 0", wordBreak: "break-all"}}>{toBreakLine(content)}</p>
+            <p style={{ margin: "24px 0", wordBreak: "break-all" }}>{toBreakLine(content)}</p>
 
             <div
               ref={mapNode}
-              style={{width: '100%', height: '200px', marginTop: "24px"}}
+              style={{ width: '100%', minHeight: '200px', marginTop: "24px" }}
               data-lat={shop['緯度']}
               data-lng={shop['経度']}
               data-navigation-control="off"
             ></div>
 
             <a className="small" href={`https://www.google.com/maps/dir/?api=1&destination=${shop['緯度']},${shop['経度']}`} target="_blank" rel="noopener noreferrer">位置情報をオンにして道順を確認</a>
-
           </>
           :
           <></>
