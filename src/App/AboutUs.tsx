f57c64b2679f2e1e734973626a5805d9b5ca96be
taDaota <143489790+taDaota@users.noreply.github.com>
2024-05-07 21:25:57 +0900

    Update AboutUs.tsx

--- src/App/AboutUs.tsx
+++ src/App/AboutUs.tsx
@@ -23,11 +23,10 @@ const Content = () => {
         <p>このウェブアプリは誰もが汲める京都市内の湧き水を紹介するマップです。経路検索はGoogle Mapsと連携してご使用ください。</p>
         <p>紹介している湧き水処は、寄付の協力や汲む量の上限があります。ルールに従ってご利用ください。</p>
         <p>このマップは「京都市文化財マネージャー育成講座」の課題の一環として作成されました。</p>
-        <h2><a href="https://www.city.kyoto.lg.jp/bunshi/page/0000321558.html" target="_blank" rel="noopener noreferrer">京都市文化財マネージャー育成講座</a></h2>
-        <p>歴史的建造物の調査や保存・活用とそれを生かしたまちづくりについて、講義と演習、修了課題で実践的に学ぶ講座です。同講座は京都市に拠点を置く認定NPO法人《古材文化の会》が主催しており、2024年度で14期生として36名を迎えました。</p>
-        <h3>古材文化の会</h3>
+        <h3><a href="https://www.city.kyoto.lg.jp/bunshi/page/0000321558.html" target="_blank" rel="noopener noreferrer">京都市文化財マネージャー育成講座</a></h3>
+        <p>歴史的建造物の調査や保存・活用とそれを生かしたまちづくりについて、講義と演習、修了課題で実践的に学ぶ講座です。2024年度は14期生として36名を迎えました。同講座は京都市に拠点を置く認定NPO法人《古材文化の会》が主催しており、</p>
+        <h3><a href="http://www.kozai.or.jp/" target="_blank" rel="noopener noreferrer">古材文化の会</a></h3>
         <p>日本の伝統的な建築様式で使用される古材の保存、活用、普及を目的としています。</p>
-        <p><a href="http://www.kozai.or.jp/" target="_blank" rel="noopener noreferrer">古材文化の会ホームページ</a></p>
         <p>※紹介している湧き水は水質検査済み、かつ作者が複数回飲んだことがある湧き水のみです。他にもたくさんあります。</p>
 
         {config.form_url && (
