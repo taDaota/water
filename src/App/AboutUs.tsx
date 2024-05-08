2be060d76c35d28d6a1dc9754bb5ae0630edaf0c
taDaota <143489790+taDaota@users.noreply.github.com>
2024-05-08 09:56:52 +0900

    Update AboutUs.tsx

--- src/App/AboutUs.tsx
+++ src/App/AboutUs.tsx
@@ -19,7 +19,7 @@ const Content = () => {
   const imageUrl = `${process.env.PUBLIC_URL}/icon-pwamap.svg`;
 
   // スムーズスクロール関数
-  const handleNavigation = (sectionId) => {
+  const handleNavigation = (sectionId: string) => {
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth', block: 'start' });
