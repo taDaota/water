aa1d2c7801524263a39864e547a727ce051a4c05
taDaota <143489790+taDaota@users.noreply.github.com>
2024-05-12 23:46:17 +0900

    Update AboutUs.tsx

--- src/App/AboutUs.tsx
+++ src/App/AboutUs.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './AboutUs.scss';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { FaBars, FaTimes } from 'react-icons/fa'; // 不要なimportを削除
 
 const Content = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -38,20 +38,19 @@ const Content = () => {
           <div className="image"><img src={imageUrl} alt="PWAMap Icon" /></div>
           <div className="logo">京都湧水マップ</div>
           <ul>
-            <li><br/>・誰でも汲める京都市内の湧き水を紹介するウェブアプリです。</li>
+            <li>・誰でも汲める京都市内の湧き水を紹介するウェブアプリです。</li>
             <li>・各所の決まりごとに従ってお水を頂きましょう。</li>
             <li>・このマップは古材文化の会が主宰する「京都市文化財マネージャー育成講座」の課題の一環として作成されました。</li>
           </ul>
         </div>
         <section id="currentLocation">
           <h3>現在位置を表示させる</h3>
-          <ul>
-            <li>1.【位置情報サービス】をオンにし、使用しているブラウザを選択します。</li>
+          <p>MAPで現在位置を表示させる手順:</p>
+          　<li>1.【位置情報サービス】をオンにし、使用しているブラウザを選択します。</li>
             <li>2.【次回または共有時に確認】または【このアプリの使用中】を選択し、【正確な位置情報】トグルスイッチがオンであることを確認</li>
             <li>3. 立ち上げ画面のMAP右上にある「⛯」アイコンをタップ</li>
             <li>4. 位置情報を使用するポップアップ画面が出れば【許可】を選択</li>
             <li>5. MAPで現在位置が青●として表示されます</li>
-          </ul>
         </section>
         <section id="directions">
           <h3>道順の確認方法</h3>
@@ -64,22 +63,18 @@ const Content = () => {
           </ul>
         </section>
         <section id="appUsage">
-          <h3>アプリのように使う</h3>
-          <ul>
-            <li>1. ディフォルトのブラウザを開く</li>
-            <li>2. 設定メニューにアクセスする
-                <ul>
-                  <li>- Safariでは画面下にある共有アイコン（四角いボックスと矢印）をタップ</li>
-                  <li>- ChromeやEdgeでは画面右上の三点リーダー（︙）をタップ</li>
-                </ul>
-            </li>
-            <li>3.【ホーム画面に追加】を選択→アイコンがホーム画面に追加されます。</li>
-          </ul>
-        </section>
-        <section id="culturalEducation">
+            <h3>アプリのように使う</h3>
+            <ul>
+              <li>1. ディフォルトのブラウザを開く</li>
+              <li>2. 設定メニューにアクセスする
+              <li>3. Safariでは画面下にある共有アイコン（四角いボックスと矢印）をタップ</li>
+              <li>4. ChromeやEdgeでは画面右上の三点リーダー（︙）をタップ</li>
+              <li>5.【ホーム画面に追加】を選択→アイコンがホーム画面に追加されます。</li>
+          </section>
+          <section id="culturalEducation">
           <h3>京都市文化財マネージャー<br/>育成講座とは</h3>
           <p>歴史的建造物の調査や保存・活用とそれを生かしたまちづくりを、講義と演習、修了課題で実践的に学ぶ講座です。修了者は京都市文化財マネージャーへ登録し、活躍することができます。2024年度は14期生として36名を迎えました。</p>
-          <h3><br>古材文化の会</h3>
+          <h3>古材文化の会</h3>
           <p>日本の伝統的な木造建築文化の継承と発展を目指し、木材や古材の利活用と地域活性化を目的とした活動を行っています。</p>
           <p><a href="http://www.kozai.or.jp/" target="_blank" rel="noopener noreferrer">古材文化の会ホームページ</a></p>
         </section>
